e555a5727c7f3160006c36941abcfb587367fdd2
KKamjangE <ajm980718@gmail.com>
2024-04-02 16:04:25 +0900

    Tailwind Variables

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,15 @@
 export default function Home() {
     return (
-        <main className="bg-gray-200 h-screen flex items-center justify-center p-5 dark:bg-gray-700">
-            <div className="bg-white w-full max-w-screen-sm shadow-lg p-5 rounded-3xl dark:bg-gray-600">
-                <div className="flex justify-between items-center">
-                    <div className="flex flex-col">
-                        <span className="text-gray-600 font-semibold -mb-1 dark:text-gray-300">
-                            In transit
-                        </span>
-                        <span className="text-4xl font-semibold dark:text-white">
-                            CoolBlue
-                        </span>
-                    </div>
-                    <div className="bg-orange-400 rounded-full size-12" />
-                </div>
-                <div className="my-2 flex items-center gap-2">
-                    <span className="bg-green-500 text-white uppercase px-2.5 py-1.5 text-xs font-medium rounded-full transition hover:bg-green-700 hover:scale-125">
-                        Today
-                    </span>
-                    <span className="dark:text-gray-100">9:30-10:30</span>
-                </div>
-                <div className="relative">
-                    <div className="bg-gray-300 w-full h-2 rounded-full absolute" />
-                    <div className="bg-green-400 w-2/3 h-2 rounded-full absolute" />
-                </div>
-                <div className="flex justify-between items-center mt-5 text-gray-600 dark:text-gray-300">
-                    <span>Expected</span>
-                    <span>Sorting center</span>
-                    <span>In transit</span>
-                    <span className="text-gray-400 dark:text-gray-500">
-                        Delivered
-                    </span>
-                </div>
+        <main className="bg-gray-200 h-screen flex items-center justify-center p-5">
+            <div className="bg-white w-full max-w-screen-sm shadow-lg p-5 rounded-3xl flex flex-col gap-2">
+                <input
+                    type="text"
+                    placeholder="Search here..."
+                    className="w-full rounded-full bg-gray-200 pl-5 h-12 ring ring-transparent focus:ring-orange-500 outline-none transition-shadow placeholder:drop-shadow"
+                />
+                <button className="bg-black text-white rounded-full h-12 active:scale-90 focus:scale-90 transition-transform font-medium">
+                    Search
+                </button>
             </div>
         </main>
     );
