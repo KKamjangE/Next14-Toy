2396fdfd440d179f4aac93f8a8b0ba7ea218eaf5
KKamjangE <ajm980718@gmail.com>
2024-04-02 17:27:55 +0900

    Group Modifiers

--- app/page.tsx
+++ app/page.tsx
@@ -6,18 +6,17 @@ export default function Home() {
                 flex flex-col gap-3
                 shadow-lg rounded-3xl"
             >
-                {["Nico", "Me", "You", "Yourself", ""].map((person, index) => (
-                    <div key={index} className="flex items-center gap-4">
-                        <div className="size-10 bg-blue-400 rounded-full" />
-                        <span className="text-lg font-medium empty:w-24 empty:h-5 empty:rounded-full empty:animate-pulse empty:bg-gray-300">
-                            {person}
-                        </span>
-                        <div className="size-6 bg-red-500 text-white flex items-center justify-center rounded-full relative animate-bounce">
-                            <span className="z-10">{index}</span>
-                            <div className="size-6 bg-red-500 rounded-full absolute animate-ping" />
-                        </div>
-                    </div>
-                ))}
+                <div className="group flex flex-col">
+                    <input
+                        className="bg-gray-100 w-full"
+                        type="text"
+                        placeholder="Write your email"
+                    />
+                    <span className="group-focus-within:block hidden">
+                        Make sure it is a valid email...
+                    </span>
+                    <button>Submit</button>
+                </div>
             </div>
         </main>
     );
