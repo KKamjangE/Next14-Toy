67559356bed11c4625da3147228d8cfefb91162e
KKamjangE <ajm980718@gmail.com>
2024-04-02 17:39:51 +0900

    JIT

--- app/page.tsx
+++ app/page.tsx
@@ -2,21 +2,13 @@ export default function Home() {
     return (
         <main className="bg-gray-200 h-screen flex items-center justify-center p-5">
             <div
-                className="bg-white w-full max-w-screen-sm p-5
+                className="bg-white w-full max-w-screen-sm p-4
                 flex flex-col gap-3
                 shadow-lg rounded-3xl"
             >
-                <div className="group flex flex-col">
-                    <input
-                        className="bg-gray-100 w-full"
-                        type="text"
-                        placeholder="Write your email"
-                    />
-                    <span className="group-focus-within:block hidden">
-                        Make sure it is a valid email...
-                    </span>
-                    <button>Submit</button>
-                </div>
+                <button className="bg-[#5b44df] w-full h-[55.12px] text-white rounded-sexy-name mt-tomato">
+                    Submit
+                </button>
             </div>
         </main>
     );
