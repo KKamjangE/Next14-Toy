fc8e84f9f552c434934dd0298467713ed01efb88
KKamjangE <ajm980718@gmail.com>
2024-05-16 09:55:56 +0900

    Style: prettier code formatting

--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function CreateAccount() {
     return (
-        <div className="flex flex-col gap-10 py-8 px-6">
+        <div className="flex flex-col gap-10 px-6 py-8">
             <div className="flex flex-col gap-2 *:font-medium">
                 <h1 className="text-2xl">안녕하세요!</h1>
                 <h2 className="text-xl">Fill in the form below to join!</h2>
@@ -14,15 +14,15 @@ export default function CreateAccount() {
                         type="text"
                         placeholder="Username"
                         required
-                        className="bg-transparent rounded-md w-full h-10 focus:outline-none ring-1 focus:ring-2 ring-neutral-200 focus:ring-orange-500 border-none placeholder:text-neutral-400"
+                        className="h-10 w-full rounded-md border-none bg-transparent ring-1 ring-neutral-200 placeholder:text-neutral-400 focus:outline-none focus:ring-2 focus:ring-orange-500"
                     />
-                    <span className="text-red-500 font-medium">
+                    <span className="font-medium text-red-500">
                         Input error
                     </span>
                 </div>
                 <button className="primary-btn h-10">Create account</button>
             </form>
-            <div className="w-full h-px bg-neutral-500" />
+            <div className="h-px w-full bg-neutral-500" />
             <div>
                 <Link
                     href={"/sms"}
--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,16 @@ import Link from "next/link";
 
 export default function Home() {
     return (
-        <div className="flex flex-col items-center justify-between min-h-screen p-5">
+        <div className="flex min-h-screen flex-col items-center justify-between p-5">
             <div className="my-auto flex flex-col items-center gap-2 *:font-medium">
                 <span className="text-9xl">🥕</span>
                 <h1 className="text-4xl">당근</h1>
                 <h2 className="text-2xl">당근 마켓에 어서오세요!</h2>
             </div>
-            <div className="flex flex-col items-center gap-3 w-full">
+            <div className="flex w-full flex-col items-center gap-3">
                 <Link
                     href={"/create-account"}
-                    className="primary-btn text-lg py-2.5"
+                    className="primary-btn py-2.5 text-lg"
                 >
                     시작하기
                 </Link>
