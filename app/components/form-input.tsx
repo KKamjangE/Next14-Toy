b2dc9f81527adb93ae284fe6a13e112b6c78c868
KKamjangE <ajm980718@gmail.com>
2024-05-16 10:52:00 +0900

    Feat: FormInput 구현

---
+++ app/components/form-input.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+interface FormInputProps {
+    type: string;
+    placeholder: string;
+    required: boolean;
+    errors: string[];
+}
+
+export default function FormInput({
+    type,
+    placeholder,
+    required,
+    errors,
+}: FormInputProps) {
+    return (
+        <div className="flex flex-col gap-2">
+            <input
+                type={type}
+                placeholder={placeholder}
+                required={required}
+                className="placeholder:text-neutral-400focus:outline-none h-10 w-full rounded-md border-none bg-transparent ring-2 ring-neutral-200 transition focus:ring-4 focus:ring-orange-500"
+            />
+            {errors.map((error, index) => (
+                <span key={index} className="font-medium text-red-500">
+                    {error}
+                </span>
+            ))}
+        </div>
+    );
+}
