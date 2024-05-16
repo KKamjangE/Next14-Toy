a0a0e800568e246c892488a54e47ae69912b1374
KKamjangE <ajm980718@gmail.com>
2024-05-16 11:23:30 +0900

    Feat: login 페이지 생성

---
+++ app/login/page.tsx
@@ -0,0 +1,30 @@
+import FormButton from "@/app/components/form-btn";
+import FormInput from "@/app/components/form-input";
+import SocialLogin from "@/app/components/social-login";
+
+export default function LoginAccount() {
+    return (
+        <div className="flex flex-col gap-10 px-6 py-8">
+            <div className="flex flex-col gap-2 *:font-medium">
+                <h1 className="text-2xl">안녕하세요!</h1>
+                <h2 className="text-xl">Log in with email and password.</h2>
+            </div>
+            <form className="flex flex-col gap-3">
+                <FormInput
+                    type="email"
+                    placeholder="Email"
+                    errors={[]}
+                    required
+                />
+                <FormInput
+                    type="password"
+                    placeholder="Password"
+                    errors={[]}
+                    required
+                />
+                <FormButton loading={false} text="Create account" />
+            </form>
+            <SocialLogin />
+        </div>
+    );
+}
