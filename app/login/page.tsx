2856afea90c739ff49fb2a18f335af089c5941db
KKamjangE <ajm980718@gmail.com>
2024-05-16 14:29:37 +0900

    Feat: login form action 생성

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,20 +3,27 @@ import FormInput from "@/app/components/form-input";
 import SocialLogin from "@/app/components/social-login";
 
 export default function Login() {
+    const handleForm = async (formData: FormData) => {
+        "use server";
+
+        console.log(formData.get("email"), formData.get("password"));
+    };
     return (
         <div className="flex flex-col gap-10 px-6 py-8">
             <div className="flex flex-col gap-2 *:font-medium">
                 <h1 className="text-2xl">안녕하세요!</h1>
                 <h2 className="text-xl">Log in with email and password.</h2>
             </div>
-            <form className="flex flex-col gap-3">
+            <form className="flex flex-col gap-3" action={handleForm}>
                 <FormInput
+                    name="email"
                     type="email"
                     placeholder="Email"
                     errors={[]}
                     required
                 />
                 <FormInput
+                    name="password"
                     type="password"
                     placeholder="Password"
                     errors={[]}
