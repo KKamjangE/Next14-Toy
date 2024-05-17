16aac7a75c73830c6bde44ee9e0ac394bb070e10
KKamjangE <ajm980718@gmail.com>
2024-05-17 11:26:35 +0900

    Feat: createAccount action 생성 및 form validation 생성

---
+++ app/create-account/actions.ts
@@ -0,0 +1,70 @@
+"use server";
+
+import { z } from "zod";
+
+const passwordRegex = new RegExp(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*?[#?!@$%^&*-]).+$/,
+);
+
+const checkPassword = ({
+    password,
+    confirm_password,
+}: {
+    password: string;
+    confirm_password: string;
+}) => password === confirm_password;
+
+const formSchema = z
+    .object({
+        username: z
+            .string({
+                invalid_type_error: "이름은 문자만 입력 가능합니다.",
+                required_error: "이름을 필수 입니다.",
+            })
+            .min(3, "너무 짧습니다.")
+            .max(10, "너무 깁니다.")
+            .toLowerCase()
+            .trim()
+            .transform((username) => `✨ ${username} ✨`),
+        email: z
+            .string({ invalid_type_error: "이메일은 문자만 입력 가능합니다." })
+            .email({ message: "이메일 형식에 맞지 않습니다." })
+            .toLowerCase(),
+        password: z
+            .string({
+                required_error: "비밀번호는 필수 입니다.",
+                invalid_type_error: "비밀번호는 문자만 입력 가능합니다.",
+            })
+            .min(10, "너무 짧습니다.")
+            .regex(
+                passwordRegex,
+                "A password must have lowercase, UPPERCASE, a number and special characters.",
+            ),
+        confirm_password: z
+            .string({
+                required_error: "비밀번호는 필수 입니다.",
+                invalid_type_error: "비밀번호는 문자만 입력 가능합니다.",
+            })
+            .min(10, "너무 짧습니다."),
+    })
+    .refine(checkPassword, {
+        // object 전체에서 에러를 가져온다.
+        message: "Both passwords should be the same!",
+        path: ["confirm_password"], // 에러의 주최자가 누구인지 알려준다.
+    });
+
+export async function createAccount(prevState: any, formData: FormData) {
+    const data = {
+        username: formData.get("username"),
+        email: formData.get("email"),
+        password: formData.get("password"),
+        confirm_password: formData.get("confirm_password"),
+    };
+
+    const result = formSchema.safeParse(data); // safeParse를 사용하면 error를 throw하지 않는다.
+    if (!result.success) {
+        return result.error.flatten();
+    } else {
+        console.log(result.data);
+    }
+}
