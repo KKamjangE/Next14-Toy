56ceface21503b8ce20cabf4962d656374932d83
KKamjangE <ajm980718@gmail.com>
2024-05-17 15:01:38 +0900

    Feat: login action 생성

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,11 +3,12 @@
 import Button from "@/app/components/button";
 import Input from "@/app/components/input";
 import SocialLogin from "@/app/components/social-login";
-import { handleForm } from "@/app/login/actions";
+import { PASSWORD_MIN_LENGTH } from "@/app/lib/constants";
+import { login } from "@/app/login/actions";
 import { useFormState } from "react-dom";
 
 export default function Login() {
-    const [state, action] = useFormState(handleForm, null);
+    const [state, dispatch] = useFormState(login, null);
 
     return (
         <div className="flex flex-col gap-10 px-6 py-8">
@@ -15,13 +16,21 @@ export default function Login() {
                 <h1 className="text-2xl">안녕하세요!</h1>
                 <h2 className="text-xl">Log in with email and password.</h2>
             </div>
-            <form action={action} className="flex flex-col gap-3">
-                <Input name="email" type="email" placeholder="Email" required />
+            <form action={dispatch} className="flex flex-col gap-3">
+                <Input
+                    name="email"
+                    type="email"
+                    placeholder="Email"
+                    required
+                    errors={state?.fieldErrors.email}
+                />
                 <Input
                     name="password"
                     type="password"
                     placeholder="Password"
                     required
+                    errors={state?.fieldErrors.password}
+                    minLength={PASSWORD_MIN_LENGTH}
                 />
                 <Button text="Log in" />
             </form>
