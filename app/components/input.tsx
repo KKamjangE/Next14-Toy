a9402dcbb04e3b07630d3f743e930fa7efbdc210
KKamjangE <ajm980718@gmail.com>
2024-05-17 13:43:30 +0900

    Rename: FormInput 컴포넌트 Input 컴포넌트로 이름 변경

--- app/components/input.tsx
+++ app/components/input.tsx
@@ -1,27 +1,23 @@
-import React from "react";
+import React, { HTMLInputTypeAttribute, InputHTMLAttributes } from "react";
 
 interface FormInputProps {
-    type: string;
-    placeholder: string;
-    required: boolean;
     errors?: string[];
     name: string;
 }
 
-export default function FormInput({
-    type,
-    placeholder,
-    required,
+export default function Input({
     errors = [],
     name,
-}: FormInputProps) {
+    ...rest
+}: FormInputProps & InputHTMLAttributes<HTMLInputElement>) {
     return (
         <div className="flex flex-col gap-2">
             <input
                 name={name}
-                type={type}
-                placeholder={placeholder}
-                required={required}
+                // type={type}
+                // placeholder={placeholder}
+                // required={required}
+                {...rest}
                 className="placeholder:text-neutral-400focus:outline-none h-10 w-full rounded-md border-none bg-transparent ring-2 ring-neutral-200 transition focus:ring-4 focus:ring-orange-500"
             />
             {errors.map((error, index) => (
