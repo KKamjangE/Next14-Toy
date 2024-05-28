d808ad7cba11aa500f257de3c3f78c040c9a9d1f
KKamjangE <ajm980718@gmail.com>
2024-05-28 16:46:54 +0900

    Feat: db 조회 test

--- lib/db.ts
+++ lib/db.ts
@@ -3,9 +3,11 @@ import { PrismaClient } from "@prisma/client";
 const db = new PrismaClient(); // 데이터베이스 생성 및 초기화
 
 async function test() {
-    const user = await db.user.create({
-        data: {
-            username: "test",
+    const user = await db.user.findMany({
+        where: {
+            username: {
+                contains: "est",
+            },
         },
     });
 
