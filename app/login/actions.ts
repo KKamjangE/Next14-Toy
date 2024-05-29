9a66935168aff6c3870b0325269f09d7e56d2bf4
KKamjangE <ajm980718@gmail.com>
2024-05-29 15:21:02 +0900

    Feat: 로그인 기능 구현

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -5,10 +5,31 @@ import {
     PASSWORD_REGEX,
     PASSWORD_REGEX_ERROR,
 } from "@/lib/constants";
+import db from "@/lib/db";
 import { z } from "zod";
+import bcrypt from "bcrypt";
+import getSession from "@/lib/session";
+import { redirect } from "next/navigation";
+
+const checkEmailExists = async (email: string) => {
+    const user = await db.user.findUnique({
+        where: {
+            email,
+        },
+        select: {
+            id: true,
+        },
+    });
+
+    return Boolean(user);
+};
 
 const formSchema = z.object({
-    email: z.string().email().toLowerCase(),
+    email: z
+        .string()
+        .email()
+        .toLowerCase()
+        .refine(checkEmailExists, "An account with this email does not exist"),
     password: z
         .string({ required_error: "Password is required" })
         .min(PASSWORD_MIN_LENGTH)
@@ -21,11 +42,39 @@ export async function login(prevState: any, formData: FormData) {
         password: formData.get("password"),
     };
 
-    const result = formSchema.safeParse(data);
+    const result = await formSchema.safeParseAsync(data);
 
     if (!result.success) {
         return result.error.flatten();
     } else {
-        console.log(result.data);
+        const user = await db.user.findUnique({
+            where: {
+                email: result.data.email,
+            },
+            select: {
+                id: true,
+                password: true,
+            },
+        });
+
+        // 비밀번호 비교
+        const ok = await bcrypt.compare(
+            result.data.password,
+            user!.password ?? "",
+        );
+
+        if (ok) {
+            const session = await getSession();
+            session.id = user!.id; // 세션 쿠키에 id 저장
+            redirect("/profile");
+        } else {
+            // zod 인 척 error 반환
+            return {
+                fieldErrors: {
+                    email: [],
+                    password: ["Wrong password."],
+                },
+            };
+        }
     }
 }
