3f47207048f0dd5bc2bd56ff3f28dbd9809d77aa
KKamjangE <ajm980718@gmail.com>
2024-05-29 18:06:30 +0900

    Feat: middleware 생성

---
+++ middleware.ts
@@ -0,0 +1,10 @@
+import getSession from "@/lib/session";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function middleware(request: NextRequest) {
+    console.log("hello");
+}
+
+export const config = {
+    matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
