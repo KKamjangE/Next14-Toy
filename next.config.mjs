a89a56b3e3e5278a9e43e749399a13884b25fa07
KKamjangE <ajm980718@gmail.com>
2024-06-04 14:11:34 +0900

    Docs: 설명 주석 작성

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
     images: {
         remotePatterns: [
+            // 외부 출처 이미지 최적화 허용
             {
                 hostname: "avatars.githubusercontent.com",
             },
