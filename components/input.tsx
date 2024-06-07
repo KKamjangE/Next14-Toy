4a0edc483e142c030e0c6eb3b65e192e50b91925
KKamjangE <ajm980718@gmail.com>
2024-06-07 14:48:14 +0900

    Refactor: Input 컴포넌트 ref 전달 가능하게 수정

--- components/input.tsx
+++ components/input.tsx
@@ -1,19 +1,23 @@
-import React, { InputHTMLAttributes } from "react";
+import React, { ForwardedRef, InputHTMLAttributes, forwardRef } from "react";
 
 interface InputProps {
     errors?: string[];
     name: string;
 }
 
-export default function Input({
-    errors = [],
-    name,
-    ...rest
-}: InputProps & InputHTMLAttributes<HTMLInputElement>) {
+const _Input = (
+    {
+        errors = [],
+        name,
+        ...rest
+    }: InputProps & InputHTMLAttributes<HTMLInputElement>,
+    ref: ForwardedRef<HTMLInputElement>,
+) => {
     return (
         <div className="flex flex-col gap-2">
             <input
                 name={name}
+                ref={ref}
                 className="placeholder:text-neutral-400focus:outline-none h-10 w-full rounded-md border-none bg-transparent ring-2 ring-neutral-200 transition focus:ring-4 focus:ring-orange-500"
                 {...rest}
             />
@@ -24,4 +28,6 @@ export default function Input({
             ))}
         </div>
     );
-}
+};
+
+export default forwardRef(_Input);
