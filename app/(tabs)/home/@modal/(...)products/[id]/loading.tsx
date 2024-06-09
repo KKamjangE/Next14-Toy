226d3bfeb3aeaa2b9e1312119caa9dafe162fc09
KKamjangE <ajm980718@gmail.com>
2024-06-10 00:10:15 +0900

    Feat: product modal 컴포넌트 구현 (WIP)

--- app/(tabs)/home/@modal/(...)products/[id]/loading.tsx
+++ app/(tabs)/home/@modal/(...)products/[id]/loading.tsx
@@ -1,33 +1,7 @@
 import CloseButton from "@/components/close-button";
-import db from "@/lib/db";
 import { PhotoIcon } from "@heroicons/react/24/solid";
-import { notFound } from "next/navigation";
 
-export default async function Modal({ params }: { params: { id: string } }) {
-    const id = Number(params.id);
-
-    if (isNaN(id)) {
-        return notFound();
-    }
-
-    const product = await db.product.findUnique({
-        where: {
-            id,
-        },
-        include: {
-            user: {
-                select: {
-                    username: true,
-                    avatar: true,
-                },
-            },
-        },
-    });
-
-    if (!product) {
-        return notFound();
-    }
-
+export default function Loading() {
     return (
         <div className="absolute left-0 top-0 z-50 flex h-full w-full items-center justify-center bg-black bg-opacity-60">
             <CloseButton />
