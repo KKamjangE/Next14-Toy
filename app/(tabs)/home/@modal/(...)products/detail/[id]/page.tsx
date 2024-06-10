a0ac433ee25336a28820525aaea772978a28dc20
KKamjangE <ajm980718@gmail.com>
2024-06-10 18:06:41 +0900

    Feat: modal UI (WIP)

--- app/(tabs)/home/@modal/(...)products/detail/[id]/page.tsx
+++ app/(tabs)/home/@modal/(...)products/detail/[id]/page.tsx
@@ -1,6 +1,9 @@
 import CloseButton from "@/components/close-button";
 import db from "@/lib/db";
-import { PhotoIcon } from "@heroicons/react/24/solid";
+import { formatToWon } from "@/lib/utils";
+import { UserIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 
 export default async function Modal({ params }: { params: { id: string } }) {
@@ -29,11 +32,50 @@ export default async function Modal({ params }: { params: { id: string } }) {
     }
 
     return (
-        <div className="absolute left-0 top-0 z-50 flex h-full w-full items-center justify-center bg-black bg-opacity-60">
+        <div className="absolute left-0 top-0 z-50 flex h-full w-full flex-col items-center justify-center bg-black bg-opacity-60">
             <CloseButton />
-            <div className="flex h-1/2 w-full max-w-screen-sm justify-center">
-                <div className="flex aspect-square items-center justify-center rounded-md bg-neutral-700 text-neutral-200">
-                    <PhotoIcon className="h-28" />
+            <div className="flex aspect-square h-1/2 w-full max-w-screen-sm flex-col justify-center">
+                <div className="relative aspect-square">
+                    <Image
+                        src={`${product.photo}/public`}
+                        alt={product.title}
+                        fill
+                        className="object-cover"
+                    />
+                </div>
+                <div className="flex flex-col bg-neutral-800 p-3">
+                    <div className="flex items-center gap-3">
+                        <div className="size-10 overflow-hidden rounded-full">
+                            {product.user.avatar !== null ? (
+                                <Image
+                                    src={`${product.user.avatar}`}
+                                    alt={product.user.username}
+                                    width={40}
+                                    height={40}
+                                />
+                            ) : (
+                                <UserIcon />
+                            )}
+                        </div>
+                        <h3>{product.user.username}</h3>
+                    </div>
+                    <div className="py-5">
+                        <h1 className="text-2xl font-semibold">
+                            {product.title}
+                        </h1>
+                        <p>{product.description}</p>
+                    </div>
+                    <div className="flex items-center justify-between">
+                        <span className="text-lg font-semibold">
+                            {formatToWon(product.price)}원
+                        </span>
+                        <Link
+                            href={``}
+                            className="rounded-md bg-orange-500 px-5 py-2.5 font-semibold text-white"
+                        >
+                            채팅하기
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
