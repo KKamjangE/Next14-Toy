6942ab52ff5a0d8d117b8262fe8ccbd84aa4f790
KKamjangE <ajm980718@gmail.com>
2024-06-13 17:28:46 +0900

    Feat: post detail page 생성

---
+++ components/like-button.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { HandThumbUpIcon as SolidHandThumbUpIcon } from "@heroicons/react/24/solid";
+import { HandThumbUpIcon as OutlineHandThumbUpIcon } from "@heroicons/react/24/outline";
+import { useOptimistic } from "react";
+import { dislikePost, likePost } from "@/app/posts/[id]/actions";
+
+interface LikeButtonProps {
+    isLiked: boolean;
+    likeCount: number;
+    postId: number;
+}
+
+export default function LikeButton({
+    isLiked,
+    likeCount,
+    postId,
+}: LikeButtonProps) {
+    const [state, reducerFn] = useOptimistic(
+        { isLiked, likeCount }, // 초기값
+        // reducer
+        (prevState, payload) => ({
+            isLiked: !prevState.isLiked,
+            likeCount: prevState.isLiked
+                ? prevState.likeCount - 1
+                : prevState.likeCount + 1,
+        }),
+    );
+
+    const onClick = async () => {
+        reducerFn(null);
+        if (isLiked) {
+            await dislikePost(postId);
+        } else {
+            await likePost(postId);
+        }
+    };
+
+    return (
+        <button
+            onClick={onClick}
+            className={`flex items-center gap-2 rounded-full border border-neutral-400 p-2 text-sm text-neutral-400 ${state.isLiked ? "border-orange-500 bg-orange-500 text-white" : ""}`}
+        >
+            {state.isLiked ? (
+                <>
+                    <SolidHandThumbUpIcon className="size-5" />
+                    <span>{state.likeCount}</span>
+                </>
+            ) : (
+                <>
+                    <OutlineHandThumbUpIcon className="size-5" />
+                    <span>공감하기 ({state.likeCount})</span>
+                </>
+            )}
+        </button>
+    );
+}
