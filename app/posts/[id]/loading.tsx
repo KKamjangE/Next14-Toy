4f6cf418fd3af5f88bdc991b9f159f86c590f4b4
KKamjangE <ajm980718@gmail.com>
2024-06-13 17:47:37 +0900

    Feat: post detail UI skeleton 생성

--- app/posts/[id]/loading.tsx
+++ app/posts/[id]/loading.tsx
@@ -1,3 +1,33 @@
 export default function Loading() {
-    return <div></div>;
+    return (
+        <div className="flex animate-pulse flex-col gap-5 p-5">
+            <div className="flex flex-col gap-5 border-b border-neutral-600 pb-5 *:rounded-md">
+                <div className="flex items-center gap-2">
+                    <div className="size-7 rounded-full bg-neutral-700" />
+                    <div className="flex flex-col gap-2 *:rounded-md">
+                        <div className="h-3 w-40 bg-neutral-700" />
+                        <div className="h-3 w-20 bg-neutral-700" />
+                    </div>
+                </div>
+                <div className="flex flex-col gap-2 *:rounded-md">
+                    <div className="h-5 w-20 bg-neutral-700" />
+                    <div className="h-5 w-40 bg-neutral-700" />
+                </div>
+                <div className="h-5 w-40 bg-neutral-700" />
+                <div className="h-5 w-40 bg-neutral-700" />
+            </div>
+            {[...Array(10)].map((_, index) => (
+                <div key={index} className="flex flex-col gap-5 *:rounded-md">
+                    <div className="flex items-center gap-2">
+                        <div className="size-7 rounded-full bg-neutral-700" />
+                        <div className="flex flex-col gap-2 *:rounded-md">
+                            <div className="h-3 w-40 bg-neutral-700" />
+                            <div className="h-3 w-20 bg-neutral-700" />
+                        </div>
+                    </div>
+                    <div className="h-5 w-80 bg-neutral-700" />
+                </div>
+            ))}
+        </div>
+    );
 }
