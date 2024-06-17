8c7a07ec64aa6e0a0894d3e5197fb93b26aed7cf
KKamjangE <ajm980718@gmail.com>
2024-06-17 14:26:23 +0900

    Feat: 안읽은 메시지 개수 표시 UI 생성

--- app/(tabs)/chat/page.tsx
+++ app/(tabs)/chat/page.tsx
@@ -1,7 +1,9 @@
 import db from "@/lib/db";
 import { getSession } from "@/lib/session";
 import { formatToTimeAge } from "@/lib/utils";
+import { unstable_cache as nextCache } from "next/cache";
 import Image from "next/image";
+import Link from "next/link";
 
 async function getChatRooms() {
     const session = await getSession();
@@ -13,8 +15,22 @@ async function getChatRooms() {
             },
         },
         select: {
-            id: true,
+            id: true, // 채팅방으로 이동하기 위한 ID
+            _count: {
+                // 안 읽은 상대방 메시지 개수
+                select: {
+                    messages: {
+                        where: {
+                            isRead: false,
+                            userId: {
+                                not: session.id,
+                            },
+                        },
+                    },
+                },
+            },
             messages: {
+                // 최신 메시지, 날짜
                 select: {
                     payload: true,
                     created_at: true,
@@ -25,6 +41,7 @@ async function getChatRooms() {
                 },
             },
             users: {
+                // 상대방 프로필
                 where: {
                     id: {
                         not: session.id,
@@ -41,14 +58,22 @@ async function getChatRooms() {
     return chatRooms;
 }
 
+const getCachedChatRooms = nextCache(getChatRooms, ["chat-rooms"], {
+    revalidate: 60,
+    tags: ["chat-rooms"],
+});
+
 export default async function Chat() {
-    const chatRooms = await getChatRooms();
-    console.log(chatRooms[0]);
+    const chatRooms = await getCachedChatRooms();
 
     return (
         <div>
             {chatRooms.map((chatRoom) => (
-                <div key={chatRoom.id}>
+                <Link
+                    key={chatRoom.id}
+                    href={`chats/${chatRoom.id}`}
+                    className="relative flex gap-3 border-b border-neutral-500 p-5 *:text-white last:border-0"
+                >
                     <Image
                         src={chatRoom.users[0].avatar!}
                         alt={chatRoom.users[0].username}
@@ -56,14 +81,25 @@ export default async function Chat() {
                         height={50}
                         className="size-12 rounded-full"
                     />
-                    <span>{chatRoom.users[0].username}</span>
-                    <span>{chatRoom.messages[0].payload}</span>
-                    <span>
-                        {formatToTimeAge(
-                            chatRoom.messages[0].created_at.toString(),
-                        )}
-                    </span>
-                </div>
+                    {chatRoom._count.messages > 0 ? (
+                        <span className="absolute left-14 top-4 h-5 w-5 rounded-full bg-red-500 text-center text-sm">
+                            {chatRoom._count.messages}
+                        </span>
+                    ) : null}
+                    <div className="flex w-full flex-col">
+                        <span className="font-semibold">
+                            {chatRoom.users[0].username}
+                        </span>
+                        <div className="flex justify-between">
+                            <span>{chatRoom.messages[0].payload}</span>
+                            <span>
+                                {formatToTimeAge(
+                                    chatRoom.messages[0].created_at.toString(),
+                                )}
+                            </span>
+                        </div>
+                    </div>
+                </Link>
             ))}
         </div>
     );
